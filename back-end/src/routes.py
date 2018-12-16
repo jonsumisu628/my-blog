06d2e59360034d2fa0391af82a7d674f925932bc
NozomiSugiyama <rioc.sugiyama@gmail.com>
2018-12-16 22:03:47 +0900

    fix

--- back-end/src/routes.py
+++ back-end/src/routes.py
@@ -3,36 +3,37 @@ import json
 from src.handler import blog, user
 
 
-class UserListEndpoint:
-    def on_get(self, req, resp, *, id):
-        resp.media = user.get_user_list()
-
-    def on_post(self, req, resp, *, id):
-        pass
-
-class UserEndpoint:
-    def on_get(self, req, resp, *, id):
-        resp.media = user.get_user_by_id(id)
-
-    def on_post(self, req, resp, *, id):
-        pass
-
-class BlogListEndpoint:
-    def on_get(self, req, resp, *, id):
-        resp.media = blog.get_blog_list()
-
-    def on_post(self, req, resp, *, id):
-        pass
-
-class BlogEndpoint:
-    def on_get(self, req, resp, *, id):
-        resp.media = blog.get_blog_by_id(id)
-
-    def on_post(self, req, resp, *, id):
-        pass
-
 def set_routes(api):
-    api.add_route("/users", UserListEndpoint)
-    api.add_route("/users/{id}", UserListEndpoint)
-    api.add_route("/blog", BlogListEndpoint)
-    api.add_route("/blog/{id}", BlogEndpoint)
+    @api.route("/users")
+    class UserListResource:
+        def on_get(self, req, resp, *, id):
+            resp.media = user.get_user_list()
+
+        def on_post(self, req, resp, *, id):
+            pass
+
+    @api.route("/users/{id}")
+    class UserResource:
+        def on_get(self, req, resp, *, id):
+            resp.media = user.get_user_by_id(id)
+
+        def on_post(self, req, resp, *, id):
+            pass
+
+    @api.route("/blog")
+    class BlogListResource:
+        def on_get(self, req, resp, *, id):
+            resp.media = blog.get_blog_list()
+
+        def on_post(self, req, resp, *, id):
+            pass
+
+    @api.route("/blog/{id}")
+    class BlogResource:
+        def on_get(self, req, resp, *, id):
+            resp.media = blog.get_blog_by_id(id)
+
+        def on_post(self, req, resp, *, id):
+            pass
+
+    return api
